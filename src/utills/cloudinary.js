ddcc6e50c15cb45dfc23f5e8fc02a91bf6921d2b
Abuzar Sheikh <abuzarssheikh3@gmail.com>
2025-04-21 11:33:07 +0530

    So now, The Backend Set-up (Yess! Setup), is completed with cloudinary and Multer

---
+++ src/utills/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLUDINARY_API_SECRET,
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localPath) return null
+        const response = await cloudinary.uploader.upload(localPath, {
+            resource_type: 'auto',
+        })
+
+        // fs.unlinkSync(localFilePath)   // After using Postman, If file is uploading successfulyy then uncomment
+        console.log('File is Uploaded on Cloudinary', response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
